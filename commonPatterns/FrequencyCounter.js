9ac0049526ee65336f197917224ef653d155f19c
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2021-03-02 15:05:41 -0500

    algorithm practice

---
+++ commonPatterns/FrequencyCounter.js
@@ -0,0 +1,10 @@
+function squared(arr, arr2){
+    if(arr.length !== arr2.length){
+        return false;
+    }
+    for(let i =0; )
+}
+
+console.log(squared([1,2,3],[4,1,9])) //true
+console.log(squared([1,2,3],[4,4,9])) //false
+console.log(squared([1,2,3],[4,9])) //false
